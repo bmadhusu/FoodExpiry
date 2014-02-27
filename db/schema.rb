1f403e489b58a556196e01f940a9ab004febd935
B. Madhusudan <madhu_lists@yahoo.com>
2014-02-27 09:04:46 -0500

    Created User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20121231192436) do
+ActiveRecord::Schema.define(version: 20140227135805) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "foods", force: true do |t|
     t.integer  "user_name_id"
@@ -24,6 +27,13 @@ ActiveRecord::Schema.define(version: 20121231192436) do
     t.date     "acquired_on"
     t.float    "price"
     t.string   "food_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
