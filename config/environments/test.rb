88937b0a7852273300a61c4bb9053370420c01d8
Madhu <madhu@BOMMASAMUDRAMs-MacBook-Air.local>
2014-03-05 15:14:20 -0500

    Sign up page created

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,8 @@ FoodExpiry::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcrypt's cost function
+  ActiveModel::SecurePassword.min_cost = true
+  
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name		"B. Madhusudan"
+		email		"bmadhusu@yahoo.com"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
